14ade3286984c57ca392cae61f0d8e8443c95e93
Amber <amberseia1212@gmail.com>
2025-07-04 22:18:52 +0900

    feat: ゲストログイン用のルーティングを設定 🔧

--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,11 @@ Rails.application.routes.draw do
     passwords: 'users/passwords',
     unlocks: 'users/unlocks'
   }
+  
+  # ゲストログイン用のルート
+  devise_scope :user do
+    post '/guest_login', to: 'users/sessions#guest_login', as: :guest_login
+  end
+  
   root "posts#index"
 end
